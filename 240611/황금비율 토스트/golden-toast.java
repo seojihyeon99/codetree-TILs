92d9f1fed20e2de205a9c01b6d694762bf7b2861
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-06-11 16:00:42 +0900

    Title: 황금비율 토스트, Time: 649ms, Memory: 34MB, Status: Passed - CodeTree

--- 240611/황금비율 토스트/golden-toast.java
+++ 240611/황금비율 토스트/golden-toast.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 /**
  * L은 포인터 좌로 이동, R은 포인터 우로 이동 => 좌우로 포인터 이동 편해야 하므로, Iterator 써서 prev, next 활용해야겠다.
  * D은 노드 제거, P는 노드 추가 => 삭제/삽입이 빈번하게 일어날 수도 있으니.. 동적배열보다는 연결리스트 써야겠다.
@@ -46,8 +47,9 @@ public class Main {
         	
         }
         
-        for(int i=0; i<l.size(); i++) {
-        	sb.append(l.get(i));
+        it = l.listIterator();
+        while(it.hasNext()) {
+        	sb.append(it.next());
         }
         System.out.println(sb);
         
