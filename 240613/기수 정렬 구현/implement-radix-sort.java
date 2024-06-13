08fffbc63a10f43b53c58a098743dc68af87bcd1
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-06-13 15:15:02 +0900

    Title: 기수 정렬 구현, Time: 429ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 240613/기수 정렬 구현/implement-radix-sort.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine()); // 원소의 개수
+        
+        // 원소 입력 받기
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 해당 자리가 0~9인 숫자 저장
+    	Queue<Integer>[] queue = new Queue[10];
+    	for(int i=0; i<10; i++) {
+    		queue[i] = new ArrayDeque<>();
+    	}
+    	
+    	// 기수 정렬
+        for(int i=1; i<=6; i++) { // i는 현재 자릿수(뒤에서부터)
+        	// 해당 자릿수가 0~9인 숫자 저장
+        	for(int j=0; j<n; j++) {
+        		int num = (arr[j]%(10*i));
+        		if(i != 1) num = num / (10*(i-1));
+        		
+        		queue[num].offer(arr[j]);
+        	}
+        	
+        	// 새로운 배열에 0~9 순서대로 들어간대로 저장ㄴ
+        	int[] newArr = new int[n];
+        	int idx = 0;
+        	for(int j=0; j<10; j++) {
+        		int size = queue[j].size();
+        		for(int k=0; k<size; k++) {
+        			newArr[idx++] = queue[j].poll();
+        		}
+        	}
+        	
+        	arr = newArr;
+        }
+        
+        // 출력
+        for(int i=0; i<n; i++) {
+        	System.out.print(arr[i] + " ");
+        }
+    }
+}
