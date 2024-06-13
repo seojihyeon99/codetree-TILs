1e556ee7a88bfe499165be609cc74ee4af4407bc
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-06-13 15:29:21 +0900

    Title: 기수 정렬 구현, Time: 872ms, Memory: 26MB, Status: Passed - CodeTree

--- 240613/기수 정렬 구현/implement-radix-sort.java
+++ 240613/기수 정렬 구현/implement-radix-sort.java
@@ -28,15 +28,15 @@ public class Main {
     	
     	// 기수 정렬
         for(int i=1; i<=6; i++) { // i는 현재 자릿수(뒤에서부터)
-        	// 해당 자릿수가 0~9인 숫자 저장
+        	// 해당 보는 자리의 값에(0~9) 따라서 0번~9번 Queue에 넣음
         	for(int j=0; j<n; j++) {
-        		int num = (arr[j]%(10*i));
-        		if(i != 1) num = num / (10*(i-1));
+        		int digit = arr[j]%(int)Math.pow(10, i);
+        		if(i != 1) digit = digit/(int)Math.pow(10, i-1);
         		
-        		queue[num].offer(arr[j]);
+        		queue[digit].offer(arr[j]);
         	}
         	
-        	// 새로운 배열에 0~9 순서대로 들어간대로 저장ㄴ
+        	//0번~9번 큐를 순회하며, 각각의 원소를 꺼내어 새로운 배열에 넣어줌
         	int[] newArr = new int[n];
         	int idx = 0;
         	for(int j=0; j<10; j++) {
@@ -46,7 +46,7 @@ public class Main {
         		}
         	}
         	
-        	arr = newArr;
+        	arr = newArr; // 새로운 배열(newArr)을 다시 시작배열(arr)로
         }
         
         // 출력
