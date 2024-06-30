90c6ff5a0867b4375adc0e1fb1f4b035aac7532c
Jihyeon <121781743+seojihyeon99@users.noreply.github.com>
2024-06-30 17:08:23 +0900

    Title: 수열 조작, Time: 755ms, Memory: 42MB, Status: Passed - CodeTree

---
+++ 240630/수열 조작/sequence-manipulation.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		int n = Integer.parseInt(br.readLine());
+		for(int i=1; i<=n; i++) {
+			q.offer(i);
+		}
+		
+		while(q.size() != 1) {
+			q.poll();
+			q.offer(q.poll());
+		}
+		
+		System.out.println(q.peek());
+	}
+}
